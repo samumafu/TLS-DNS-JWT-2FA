94e0e37be0ce0f0607a76b6168ba5464e13d691a
kami1185 <acde100@gmail.com>
2026-03-23 12:37:12 -0500

    EC2 configuration

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Entregar el HTML
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// API
+app.get('/api/status', (req, res) => {
+    res.json({
+        mensaje: "MEnsaje probando tls y nginx",
+        protocolo_interno: "HTTP",
+        protocolo_externo: "HTTPS (TLS 1.3)"
+    });
+});
+
+// Escuchamos en el puerto 3000 (puerto interno)
+app.listen(3000, () => {
+    console.log('🚀 Backend escuchando en puerto 3000');
+});
